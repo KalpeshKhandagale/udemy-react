e36b38848355c55e5a5a08e84368f756f3b6c214
Kalpesh Khandagale <kalpesh@brandmovers.in>
2022-12-02 13:12:49 +0530

    Initial commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+
+const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        // console.log('Expense.js');
+        // console.log(selectedYear);
+        setFilteredYear(selectedYear);
+    }
+
+    return (
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter select={filteredYear} onChangeFilter={filterChangeHandler} /> 
+                {props.items.map(expense => 
+                    <ExpenseItem 
+                        key={expense.id}
+                        title={expense.title} 
+                        amount={expense.amount} 
+                        date={expense.date} />
+                )}
+            </Card>
+        </div>
+    )
+}
+
+export default Expenses;
