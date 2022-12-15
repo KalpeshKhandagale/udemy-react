c990d07f47bd3daa282255265dff9898721a8cbb
Kalpesh Khandagale <kalpesh@brandmovers.in>
2022-12-15 18:23:22 +0530

    adding conditional return statement

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpenses = (props) => {
+    const [isEditing, setIsEditing] = useState(false)
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData);
+        setIsEditing(false)
     }
+
+    const startEditingHandler = () => {
+        setIsEditing(true)
+    }
+
+    const stoptEditingHandler = () => {
+        setIsEditing(false)
+    }
+
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isEditing && <button onClick={startEditingHandler} type="submit">New Expense</button>}
+            {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stoptEditingHandler} />}
         </div>
     ) 
 }
